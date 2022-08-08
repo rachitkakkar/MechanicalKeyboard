ea8994988a6f548f6642759ee835e434bee015f3
Rachit Kakkar <rachitkakkar@Rachits-Air.lan1>
2022-08-08 02:20:30 -0500

    Intial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mechanical Keyboard
+Make any keyboard sound like a mechanical keyboard with this python script! I came up with the idea by myself, and it works by playing one of 5 random mechanical keyboard sounds when any key is pressed. It took my 30 minutes to make, and is similar to the program Mechvibes, which I found out about after.
+
+# Install
+Simply clone this repo and install all the requirements in `requirements.txt` using pip. Tested with Python 3.9.13 and MacOS.
+
+# Usage
+`python3 mechanical_keyboard.py`
+
+Blog post in the works!
---
+++ mechanical_keyboard.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Author: Rachit Kakkar
+# Last Updated: August 7th, 2022
+# Description: Make any keyboard sound like a mechanical one by playing a sound on a keypress
+from playsound import playsound
+from pynput.keyboard import Listener
+from random import randint
+
+released = True # Keep a variable to determine whether a key has been released
+def on_press(key):
+    global released
+    if (released): # Only play the first time a key has been pressed to avoid repeat sounds when a key is held
+        playsound(f'sounds/{randint(1, 5)}.mp3') # Play one of 5 random sounds!
+        released = False
+
+def on_release(key): # Set released to true on key release
+    global released
+    released = True
+
+while True:
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+    print('MechanicalKeyboard by Rachit Kakkar has launched!')
+    print('You should hear sounds now, if on MacOS please accept permissions!')
